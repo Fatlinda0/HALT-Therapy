deb79cf346293483baf5622511b5189f51acdad0
Fatlinda0 <fb55487@ubt-uni.net>
2024-01-14 17:35:05 +0100

    Fatlinda

--- support.php
+++ support.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Help</title>
+    <title>Support</title>
     <link rel="stylesheet" href="css/quiz.css">
     <script src="https://kit.fontawesome.com/5a7ceed978.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Corinthia:wght@700&display=swap" rel="stylesheet">
